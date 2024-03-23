ac5a69fe6213d7b66f40b9730981cd382ea05bab
emrhntpcc1 <154905679+emrhntpcc1@users.noreply.github.com>
2024-03-23 16:20:14 +0300

    Fibonacci Sayısı
    Fibonacci sayısını recursive fonksiyon ile yaptım.

---
+++ FibonacciSayisiRecursive.java
@@ -0,0 +1,21 @@
+package MetotlarFonksiyonlar;
+
+public class FibonacciSayisiRecursive {
+    static int fib(int n){
+       if (n <= 0){
+           return 0;
+       }
+       else {
+           if ((n == 1) || (n == 2)){
+               return 1;
+           }
+           else{
+               return fib(n - 1) + fib(n - 2);
+           }
+       }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+}
