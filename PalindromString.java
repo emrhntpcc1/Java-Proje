fe80f7289f6ec491484d93acbaec2ed614d4dece
emrhntpcc1 <154905679+emrhntpcc1@users.noreply.github.com>
2024-04-29 15:27:18 +0300

    Palindrom
    Verilen stringin palindrom olup olmadığını kontrol eder.

---
+++ PalindromString.java
@@ -0,0 +1,19 @@
+package Diziler.MiniProjeler;
+
+public class PalindromString {
+    public static boolean isPalindrom(String str){
+        int i = 0;
+        int j = str.length()-1;
+        while (i < j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("ey edip adanada pide ye"));
+    }
+}
